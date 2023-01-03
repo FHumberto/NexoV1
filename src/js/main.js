dbfa505d292b74f51b2c121f6244794659658ed0
Humberto Guedes <franciscohguedes@gmail.com>
2023-01-03 05:38:18 -0300

    add: seção habilidades e ajustes na responsividade

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,43 @@
+//* REFERÊNCIAS
 const mobileNav = document.querySelector('.mobile-nav');
 const navLinks = document.getElementById('mobile-nav-links');
+const skillsProgress = document.querySelectorAll('[name="skillProgress"]');
 
-function toggleMobileNav() {
-    if (
-        mobileNav.style.display === 'block' &&
-        navLinks.style.display === 'block'
-    ) {
+function toggleMobileNav()
+{
+    if (mobileNav.style.display === 'block' && navLinks.style.display === 'block')
+    {
         mobileNav.style.display = 'none';
         navLinks.style.display = 'none';
-    } else {
+    }
+    else
+    {
         mobileNav.style.display = 'block';
         navLinks.style.display = 'block';
     }
 }
 
-navLinks.addEventListener('click', () => {
+navLinks.addEventListener('click', () =>
+{
     toggleMobileNav();
+});
+
+//* SKILLS
+const skillsLevel = [7, 6, 6, 7, 4, 7, 6];
+
+let count = 0;
+skillsProgress.forEach((sk) =>
+{
+    for (let i = 0; i < 10; i++)
+    {
+        if (i < skillsLevel[count])
+        {
+            sk.insertAdjacentHTML('beforeend', '<div class="bg-crimson"></div>');
+        }
+        else
+        {
+            sk.insertAdjacentHTML('beforeend', '<div class="bg-gray"></div>');
+        }
+    }
+    count++;
 });
