96356f0ea374a746015a1136b20896a2c08d08bd
Humberto Guedes <franciscohguedes@gmail.com>
2023-01-03 18:50:17 -0300

    add: seção de projetos

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@
 const mobileNav = document.querySelector('.mobile-nav');
 const navLinks = document.getElementById('mobile-nav-links');
 const skillsProgress = document.querySelectorAll('[name="skillProgress"]');
+const projectGrid = document.querySelector('.project-grid')
 
 function toggleMobileNav()
 {
@@ -41,3 +42,46 @@ skillsProgress.forEach((sk) =>
     }
     count++;
 });
+
+
+//* HABILIDADES
+async function getApiGitHub()
+{
+    try
+    {
+        const res = await fetch('https://api.github.com/users/FHumberto/repos');
+        if (!res.ok)
+        {
+            throw new Error(res.status);
+        }
+
+        const data = await res.json();
+
+        // filtra os repositórios
+        const filteredData = data.filter(data => data.name.startsWith("PES-PF"));
+
+        // faz uma requisição para cada repositório filtrado
+        for (const repo of filteredData)
+        {
+            const projectInfoRes = await fetch(`https://raw.githubusercontent.com/FHumberto/${repo.name}/main/project/info.json`);
+            const projectInfo = await projectInfoRes.json();
+
+            projectGrid.innerHTML += `
+                <div class="project-card">
+                <h3 class="project-card-tittle">${projectInfo.name}</h3>
+                <img class="project-card-img" src="https://raw.githubusercontent.com/FHumberto/${repo.name}/main/project/demo.gif" alt="Screen do ${projectInfo.name}" />
+                <div class="project-card-inner">
+                    <p>${projectInfo.description}</p>
+                    <h4>Tecnologias Utilizadas:</h4>
+                    <p>${projectInfo.tech}</p>
+                    <a class="project-card-inner-link" href="https://github.com/FHumberto/${repo.name}/">Visualizar</a>
+                </div>
+                </div>`;
+        }
+    } catch (e)
+    {
+        console.log(e);
+    }
+}
+
+getApiGitHub();
